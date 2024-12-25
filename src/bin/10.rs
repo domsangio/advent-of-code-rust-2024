7d0715b9b82e7ade1fae3e8f2ff2f789913ddc10
domsangio <domenic_sangiovanni@yahoo.com>
2024-12-25 09:34:23 -0500

    Started 10

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,6 +1,31 @@
 advent_of_code::solution!(10);
 
+fn parse_input(input: &str) -> Vec<Vec<u8>> {
+    input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| c.to_digit(10).unwrap() as u8)
+                .collect()
+        })
+        .collect()
+}
+
+fn is_in_bounds(grid: &Vec<Vec<u8>>, x: i16, y: i16)
+
+fn get_valid_neighbors(grid: &Vec<Vec<u8>>) -> HashMap<(usize, usize), u8> {
+    for i in 0..grid.len() {
+        for j in grid[0].len() {
+
+        }
+    }
+}
+
 pub fn part_one(input: &str) -> Option<u64> {
+    let topo = parse_input(input);
+
+
+
     None
 }
 
